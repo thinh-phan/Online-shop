3e1bda062c8922de2b93cfe4549acc58e40cddca
thinh-phan <phanquangthinh1997@gmail.com>
2020-08-25 00:23:48 +0700

    Project scalffolding

--- shop/models.py
+++ shop/models.py
@@ -1,20 +1,26 @@
 from django.db import models
 from django.urls import reverse
-# Create your models here.
+
+
 class Category(models.Model):
     name = models.CharField(max_length=200,
                             db_index=True)
     slug = models.SlugField(max_length=200,
                             unique=True)
+
     class Meta:
         ordering = ('name',)
         verbose_name = 'category'
         verbose_name_plural = 'categories'
+
     def __str__(self):
         return self.name
+
     def get_absolute_url(self):
         return reverse('shop:product_list_by_category',
                        args=[self.slug])
+
+
 class Product(models.Model):
     category = models.ForeignKey(Category,
                                  related_name='products',
@@ -28,11 +34,14 @@ class Product(models.Model):
     available = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+
     class Meta:
         ordering = ('name',)
         index_together = (('id', 'slug'),)
+
     def __str__(self):
         return self.name
+
     def get_absolute_url(self):
         return reverse('shop:product_detail',
                        args=[self.id, self.slug])
--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, get_object_or_404
+from cart.forms import CartAddProductForm
 from .models import Category, Product
+
+
 def product_list(request, category_slug=None):
     category = None
     categories = Category.objects.all()
@@ -12,11 +15,16 @@ def product_list(request, category_slug=None):
                   {'category': category,
                    'categories': categories,
                    'products': products})
+
+
 def product_detail(request, id, slug):
     product = get_object_or_404(Product,
                                 id=id,
                                 slug=slug,
                                 available=True)
+    cart_product_form = CartAddProductForm()
+
     return render(request,
                   'shop/product/detail.html',
-                  {'product': product})                   
+                  {'product': product,
+                   'cart_product_form': cart_product_form})
